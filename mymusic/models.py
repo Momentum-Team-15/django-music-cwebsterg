1b56178001295d6fa69a9c02218d1d910f3f8ca8
Webster Grimes <cwebsterg@mac.com>
2022-10-24 00:43:14 -0400

    detail_album updates model updates

--- mymusic/models.py
+++ mymusic/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
     location = models.CharField(max_length=30, blank=True)
     birthdate = models.DateField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['first_name', 'last_name']
@@ -30,8 +30,8 @@ class Album(models.Model):
     # ForeignKey representsa 1:many relationship (O2M)
     # the one is the field and the many are from the class
     # it is defined on
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['title']
@@ -42,8 +42,8 @@ class Album(models.Model):
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -52,8 +52,8 @@ class Artist(models.Model):
 class Song(models.Model):
     song = models.CharField(max_length=200)
     album = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='songs')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.song} by {Artist.objects.artist}"
@@ -62,8 +62,8 @@ class Song(models.Model):
 class Playlist(models.Model):
     playlist = models.CharField(max_length=100)
     user = models.ForeignKey('User', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.playlist}"
